d6c5f0b44c151d22e75cced1d704dda1297fd97e
Va2 <vadim.brieven@gmail.com>
2019-04-11 15:01:08 +0200

    register_post_type() - Update the view (html) made in our 'recipe' metabox

--- wp-content/plugins/ratatouille/resources/views/metaboxes/recipe-detail.html.php
+++ wp-content/plugins/ratatouille/resources/views/metaboxes/recipe-detail.html.php
@@ -1,1 +1,13 @@
-<h3>Hello</h3>
+<table class="form-table">
+    <tr>
+        <th><?php _e("Temps de préparation"); ?></th>
+        <td>
+            <select name="rat_time_preparation" id="rat_time_preparation">
+                <option value="">--</option>
+                <option value="10-15">10 - 15 min</option>
+                <option value="10-15">15 - 30 min</option>
+                <option value="10-15">30 - 45 min</option>
+            </select>
+        </td>
+    </tr>
+</table>
