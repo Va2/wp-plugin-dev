38498ff9cd7e51198d6fd7c073eb79142169dd8a
Va2 <vadim.brieven@gmail.com>
2019-04-11 11:37:41 +0200

    register_post_type() - Creating an alias for RecipePostType.php class

--- wp-content/plugins/ratatouille/ratatouille.php
+++ wp-content/plugins/ratatouille/ratatouille.php
@@ -1,4 +1,8 @@
 <?php
+// On utilise le namespace ici, l'avantage de cette écriture là c'est qu'on créer un 'ALIAS' avec la commande 'as' ce qui fait qu'a la ligne 24 on peut simplement écrire RecipePostType::class, pour l'exemple j'ai préciser que l'alias était RecipePostType mais a vrai dire c'est nécéssaire que si on modifie le nom de la class si on veut garder un Alias qui porte le meme nom que la class il ne faut pas le préciser si on écrivait :
+// use App\Features\PostTypes\RecipePostType cela reviendrait au même un créer automatiqement un alias du même nom si on ne le précise pas.
+use App\Features\PostTypes\RecipePostType;
+
 /**
  * Plugin Name:     Ratatouille
  * Plugin URI:      PLUGIN SITE HERE
@@ -18,5 +22,4 @@
 // http://php.net/manual/fr/function.require-once.php
 require_once('App/Features/PostTypes/RecipePostType.php');
 
-// Appel de la class venant du namespace qu'on a définit dans le fichier RecipePostType.php ,l'avantage c'est qu'on a redéfinit le nom de notre class qui était RecipePostType par App\Features\PostTypes\RecipePostType il y a maintenant presque aucune chance qu'un autre plugin utilise le meme nom de class et donc il n'y aura pas de conflit.
-add_action('init',[App\Features\PostTypes\RecipePostType::class, 'register']);
+add_action('init',[RecipePostType::class, 'register']);
