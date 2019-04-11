47f7620b5ba44280cc42f9fb035a4eed5cef49ac
Va2 <vadim.brieven@gmail.com>
2019-04-11 12:43:02 +0200

    register_post_type() - Initializing a taxnomy with default values

--- wp-content/plugins/ratatouille/ratatouille.php
+++ wp-content/plugins/ratatouille/ratatouille.php
@@ -2,6 +2,7 @@
 // On utilise ('use') ici un namespace (celui qu'on créer ligne 4 dans autoload.php), l'avantage de cette écriture là c'est qu'on créer un 'ALIAS' avec la commande 'as' ce qui fait qu'a la ligne 25 on peut simplement écrire RecipePostType::class. Pour l'exemple j'ai précisé que l'alias était RecipePostType mais a vrai dire c'est nécéssaire que si on modifie le nom de la class si on veut garder un Alias qui porte le meme nom que la class il ne faut pas le préciser si on écrivait :
 // use App\Features\PostTypes\RecipePostType cela reviendrait au même un créer automatiqement un alias du même nom si on ne le précise pas.
 use App\Features\PostTypes\RecipePostType;
+use App\Features\Taxonomies\RecipeTaxonomy;
 
 /**
  * Plugin Name:     Ratatouille
@@ -21,5 +22,5 @@ use App\Features\PostTypes\RecipePostType;
 // On require_once le fichier autload.php qui lui fera un require_once pour toutes les class qui ont besoin d'être chargées.
 require_once('autoload.php');
 
-// require_once('App/Features/PostTypes/RecipePostType.php');
 add_action('init',[RecipePostType::class, 'register']);
+add_action('init', [RecipeTaxonomy::class, 'register']);
