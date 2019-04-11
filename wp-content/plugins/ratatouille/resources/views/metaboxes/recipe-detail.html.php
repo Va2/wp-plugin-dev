841acc40f02bac32fa96ec27d7b4dca9a4b0fb5d
Va2 <vadim.brieven@gmail.com>
2019-04-11 16:20:39 +0200

    register_post_type() - We extend the number of parameters of our helper view on the opportunity to fill this second parameter with the data retrieved from the database. In the view (where we sent the data) we display it with the key we defined.

--- wp-content/plugins/ratatouille/resources/views/metaboxes/recipe-detail.html.php
+++ wp-content/plugins/ratatouille/resources/views/metaboxes/recipe-detail.html.php
@@ -3,7 +3,8 @@
         <th><?php _e("Temps de préparation"); ?></th>
         <td>
             <select name="rat_time_preparation" id="rat_time_preparation">
-                <option value="">--</option>
+                <!-- Maintenant que le tableau a été traité et renvoyé dans cette vue on peut se servir de la clef devant laquelle on rajoute un "$" pour récupérer la valeur qui lui à été attribuée. -->
+                <option value=""><?php echo $preparation_time ?></option>
                 <option value="10-15">10 - 15 min</option>
                 <option value="15-30">15 - 30 min</option>
                 <option value="30-45">30 - 45 min</option>
