6c520b116426675ef36251674d649fe5e6cfd017
Va2 <vadim.brieven@gmail.com>
2019-04-11 16:32:36 +0200

    register_post_type() - Use of a helper to sanitize data recovered from the database.

--- wp-content/plugins/ratatouille/helpers.php
+++ wp-content/plugins/ratatouille/helpers.php
@@ -14,3 +14,21 @@ function view($path, $data = [])
     // Cette function (= helper), me permet de faire un include plus rapidement. Je récupère juste le chemin du fichier à partir du dossier views sans l'extention dans le fichier RecipeDetailsMetabox.php (ligne 36) que j'envoi en paramètre. Ce chemin est envoyé dans la variable $path, puis je complète le chemin avec ma variable global et l'extention.
     include(RAT_VIEW_DIR . $path . '.html.php');
 }
+
+/**
+ * Extrait la valeur dans un tableau si la valeur existe dans ce tableau.
+ * Cela permet de ne pas avoir d'erreur lorsque l'on créer un nouveau post.
+ *
+ * @param string $key la meta key dans la base de donnée
+ * @param array $data le tableau resultant de get_post_meta
+ * @return void
+ */
+function extract_data_attr(string $key, array $data)
+{
+    // Vérification que la clé exist bien dans le tableau
+    if (array_key_exists($key, $data)) {
+        // On renvoi la valeur et on en profite pour échapper la valeur pour la sécurité
+        return esc_attr($data[$key][0]);
+    }
+    return '';
+}
--- wp-content/plugins/ratatouille/resources/views/metaboxes/recipe-detail.html.php
+++ wp-content/plugins/ratatouille/resources/views/metaboxes/recipe-detail.html.php
@@ -4,7 +4,7 @@
         <td>
             <select name="rat_time_preparation" id="rat_time_preparation">
                  <!-- On passe notre clef de $preparation_time à $time car on utilise une function compact dans RecipeDetailsMetabox qui elle génère une clef du même nom que la variable passée. -->
-                <option value=""><?= $time ?></option>
+                <option value="<?= $time ?>"><?= $time ?></option>
                 <option value="10-15">10 - 15 min</option>
                 <option value="15-30">15 - 30 min</option>
                 <option value="30-45">30 - 45 min</option>
