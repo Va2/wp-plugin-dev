4ca907fda49969c1381e128877a10e3ee721b0ed
Va2 <vadim.brieven@gmail.com>
2019-04-12 09:55:34 +0200

    register_post_type() - Creation of a new folder 'Widgets' in which we create a file containing a class allowing to add a very basic personalized Widget

--- wp-content/plugins/ratatouille/ratatouille.php
+++ wp-content/plugins/ratatouille/ratatouille.php
@@ -17,5 +17,5 @@
 // On require_once le fichier autload.php qui lui fera un require_once pour toutes les class qui ont besoin d'être chargées.
 require_once('autoload.php');
 
-// J'inclus le fichier bootstrap qui lui contient tous les require_once, le but est de structurer nos fichiers,nos dossier et de faire les appels de manière structurés dans les bons fichiers en se basant sur la structure et le fonctionnement de Laravel pour que vous ayez déjà une première approche avec la structure de Laravel.
+// J'inclus le fichier bootstrap qui lui contient tous les require_once, le but est de structurer nos fichiers,nos dossier et de faire les appels de manière structurés dans les bons fichiers en se basant sur la structure et le fonctionnement de Laravel.
 require_once('bootstrap.php');
